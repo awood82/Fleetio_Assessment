bbf24512a6afc31977cc53d86054948564a42856
Andrew Wood <ajwood1@gmail.com>
2022-11-10 15:45:32 -0800

    I ran into an issue w/ Compose library versions. Fixed by using compatible versions from the BOM list: https://developer.android.com/jetpack/compose/setup#bom-version-mapping

--- app/src/main/java/com/androidandrew/fleetio_assessment/data/Vehicle.kt
+++ app/src/main/java/com/androidandrew/fleetio_assessment/data/Vehicle.kt
@@ -3,7 +3,7 @@ package com.androidandrew.fleetio_assessment.data
 data class Vehicle(
     val id: Long,
     val name: String,
-    val thumbnailUrl: String?,
-    val make: String?,
-    val model: String?
+    val thumbnailUrl: String? = null,
+    val make: String? = null,
+    val model: String? = null
 )
--- app/src/main/java/com/androidandrew/fleetio_assessment/ui/main/MainScreen.kt
+++ app/src/main/java/com/androidandrew/fleetio_assessment/ui/main/MainScreen.kt
@@ -1,9 +1,14 @@
 package com.androidandrew.fleetio_assessment.ui.main
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.androidandrew.fleetio_assessment.data.Vehicle
 import com.androidandrew.fleetio_assessment.ui.theme.Fleetio_AssessmentTheme
 
 @Composable
@@ -15,8 +20,43 @@ fun MainScreen(
 }
 
 @Composable
+fun VehicleItem(
+    vehicle: Vehicle,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Row() {
+            Image(
+                painter = painterResource(android.R.drawable.ic_dialog_info),
+                contentDescription = null
+            )
+        }
+        Row() {
+            Column() {
+                Text(text = vehicle.name)
+                if (vehicle.make != null) {
+                    Text(text = vehicle.make)
+                }
+                if (vehicle.model != null) {
+                    Text(text = vehicle.model)
+                }
+            }
+        }
+    }
+}
+
+@Composable
 fun NetworkResults(results: String) {
     Text(text = results)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun VehicleItemPreview() {
+    Fleetio_AssessmentTheme {
+        val vehicle = Vehicle(id = 1, name = "A vehicle", make = "Volkswagen", model = "Touareg")
+        VehicleItem(vehicle = vehicle)
+    }
 }
 
 @Preview(showBackground = true)
